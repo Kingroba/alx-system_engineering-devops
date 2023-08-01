aa1aa53b581a7201da73c2e0f6dd49809ecef1a4
Robel Bekele <robba836@gmail.com>
2023-08-01 11:25:32 -0700

    task 2 2nd update

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/env ruby
+
+# This Ruby script takes a command-line argument and performs two regular expression scans on it.
+
+# The first scan looks for occurrences of "htn" in the input argument and prints them.
 puts ARGV[0].scan(/htn/).join
+
+# The second scan looks for occurrences of "hbtn" in the input argument and prints them.
 puts ARGV[0].scan(/hbtn/).join
+
+# End of the script
+
