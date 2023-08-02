217bd9f6d3a31bad0f1a9797da0af14582985959
Robel Bekele <robba836@gmail.com>
2023-08-02 12:07:52 -0700

    task 0 updated

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/env ruby
-#Match "School" in the input string
+
+# Match "School" in the input string
 matches = ARGV[0].scan(/School/)
+
+# Output the results
+if matches.empty?
+  puts "No match found with 'School'"
+else
+  puts "Match found with 'School': #{matches.join(", ")}"
+end
+
