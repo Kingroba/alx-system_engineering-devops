8a02a8041307c91cb32a1b24be8974915810a26d
Robel Bekele <robba836@gmail.com>
2023-08-02 12:12:56 -0700

    task 0 3rd updated

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,8 +1,4 @@
 #!/usr/bin/env ruby
 
-# Match "School" in the input string and concatenate the matches
-matches = ARGV[0].scan(/School/).join
-
-# Output the result
-puts "Match found with 'School': #{matches}"
+puts ARGV[0].scan(/[S]chool/).join
 
