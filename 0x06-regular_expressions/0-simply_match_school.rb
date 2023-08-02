fac81a22ac5d019990814de0521610bcac37d38a
Robel Bekele <robba836@gmail.com>
2023-08-02 12:21:09 -0700

    task 0 5rd updated

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/env ruby
 
+# Check if there is an argument passed while running the script
+if ARGV.empty?
+  puts "Neha loves School, that is why she is a School mentor :)"
+  exit
+end
+
+# Use the `scan` method with the regular expression to find matches and join them
 puts ARGV[0].scan(/School/).join
 
