851e0ef66a5d04f1f525ebea2fe2cb5bbb0e0f57
Robel Bekele <robba836@gmail.com>
2023-08-03 06:26:54 -0700

    task 0 4th updated

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,22 +1,26 @@
 #!/usr/bin/env ruby
 require 'onigmo'
 
+def match_school(input)
+  # Use the `match` method with the Oniguruma regular expression engine to find the first match
+  regex = Onigmo::Regexp.new('School')
+  match_data = regex.match(input)
+
+  # Check if there is a match and return the matched substring
+  if match_data
+    matched_substring = match_data.to_s
+    puts "Match: #{matched_substring}"
+    puts "Length: #{matched_substring.length} chars long"
+  else
+    puts "No match found for 'School'"
+  end
+end
+
 # Check if there is an argument passed while running the script
 if ARGV.empty?
   puts "Neha loves School, that is why she is a School mentor :)"
-  exit
-end
-
-# Use the `match` method with the Oniguruma regular expression engine to find the first match
-regex = Onigmo::Regexp.new('School')
-match_data = regex.match(ARGV[0])
-
-# Check if there is a match and print the result
-if match_data
-  matched_substring = match_data.to_s
-  puts matched_substring
-  puts "(#{matched_substring.length} chars long)"
 else
-  puts "No match found for 'School'"
+  input = ARGV[0]
+  match_school(input)
 end
 
