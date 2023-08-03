17587af680884b21f8081b4ca90873328199ee22
Robel Bekele <robba836@gmail.com>
2023-08-03 09:44:38 -0700

    task 100 updated

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:)\S*(?<!\])|(?<=to:).?\d{10,11}|(?<=flags:)\S*(?<!\])/).join(',')
+puts ARGV[0].scan(/(?<=from:)\+?\d+(?=\])|(?<=to:)\+?\d{10,11}|(?<=flags:)\S+(?=\])/).join(',')
+
