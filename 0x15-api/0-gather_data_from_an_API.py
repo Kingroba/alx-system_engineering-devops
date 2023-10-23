f73abaab9e6398ea6ce3c5cc1a499385a0f4f827
Robel Bekele <robba836@gmail.com>
2023-10-23 19:25:49 +0000

    task 0 revised

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,50 +3,28 @@
 This is the JSON placeholder api to query data about an employee
 """
 
-import requests
+from requests import get
 from sys import argv
 
-def get_employee_data(employee_id):
+def get_employee_data(user_id):
     main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = f'{main_url}/todos?userId={employee_id}'
-    user_url = f'{main_url}/users/{employee_id}'
+    todo_url = f"{main_url}/user/{user_id}/todos"
+    name_url = f"{main_url}/users/{user_id}"
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
 
-    try:
-        todo_response = requests.get(todo_url)
-        user_response = requests.get(user_url)
+    todo_num = len(todo_result)
+    todo_complete = len([todo for todo in todo_result if todo.get("completed")])
+    name = name_result.get("name")
 
-        todo_response.raise_for_status()
-        user_response.raise_for_status()
-
-        todo_result = todo_response.json()
-        user_result = user_response.json()
-
-        todo_num = len(todo_result)
-        todo_complete = len([todo for todo in todo_result if todo.get("completed")])
-        name = user_result.get("name")
-
-        return name, todo_complete, todo_num, todo_result
-
-    except requests.exceptions.HTTPError as errh:
-        print ("HTTP Error:", errh)
-    except requests.exceptions.ConnectionError as errc:
-        print ("Error Connecting:", errc)
-    except requests.exceptions.Timeout as errt:
-        print ("Timeout Error:", errt)
-    except requests.exceptions.RequestException as err:
-        print ("Something went wrong:", err)
+    return name, todo_complete, todo_num, todo_result
 
 if __name__ == '__main__':
-    if len(argv) != 2:
-        print("Usage: {} <employee_id>".format(argv[0]))
-        exit(1)
+    user_id = argv[1]
+    name, todo_complete, todo_num, todo_result = get_employee_data(user_id)
 
-    employee_id = argv[1]
-    name, todo_complete, todo_num, todo_result = get_employee_data(employee_id)
-
-    if name is not None:
-        print("Employee {} is done with tasks({}/{}):".format(name, todo_complete, todo_num))
-        for todo in todo_result:
-            if todo.get("completed"):
-                print("\t{}".format(todo.get("title")))
+    print(f"Employee {name} is done with tasks ({todo_complete}/{todo_num}):")
+    for todo in todo_result:
+        if todo.get("completed"):
+            print(f"\t{todo.get('title')}")
 
